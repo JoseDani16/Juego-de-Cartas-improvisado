053eb1600eb7e11e1c2158089c27992a52db11ff
José Suárez <josedanielsuarezchaves@gmail.com>
2022-08-28 21:50:20 -0300

    para entrega final

--- js/nokedex.js
+++ js/nokedex.js
@@ -4,15 +4,12 @@ const nokemonsFetch = (i) => {
     fetch('../json/nokemons.json')
     .then ((res) => res.json())
     .then ((data) => {
-            console.log(data)
             for (let i = 0; i < 27; i++) {
-                arregloAux.push(data.results[i])
+                arregloAux.push(data[i])
             }
-            console.log(arregloAux[7].sprite)
             const urlDeImagen = arregloAux.map((item) => {
-                return '<div class="itemNokedex"><p class="textoNokedex">'+item.nombre+'</p><img src="'+item.sprite+'" width="120px" height="160px"></img></div>'
+                return '<div class="itemNokedex"><p class="textoNokedex">'+item.atributo+'</p><p class="textoNokedex">'+item.nombre+'</p><img src="'+item.sprite+'" width="120px" height="160px"></img></div>'
             })
-            console.log(urlDeImagen)
             const urlListo = urlDeImagen.join('')
             nokemonContainer.innerHTML = urlListo
 })
